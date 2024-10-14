567c4949d6f854136548d8f0916ceeefea156724
thiagobergami <thiagobergamig@gmail.com>
2024-10-14 16:10:12 -0300

    Snippet view page

---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  const snippet = await db.snipeet.findFirst({
+    where: { id: parseInt(props.params.id) },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
+}
